0411ef24b1470cfdd535e4640393d4db379b2be8
mr-emeka <speak2c.emeka@gmail.com>
2020-11-06 23:07:25 +0100

    hosted web url

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+const Error = React.lazy(() => import("./component/views/Error"));
+const ViewHome = React.lazy(() => import("./component/views/Home"));
+const Room = React.lazy(() => import("./component/views/Room"));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/" exact render={(props) => <ViewHome {...props} />} />
+          <Route
+            path="/room/:roomId"
+            exact
+            render={(props) => <Room {...props} />}
+          />
+          <Route path="*" component={Error} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/component/views/Error/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div style={{ marginTop: 10, marginLeft: 10 }}>
+      <h1>Error</h1>
+    </div>
+  );
+};
+export default Error;
