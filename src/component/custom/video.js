dd3aee1ca988fdd4a926bfee561c62f9cbecc84c
mr-emeka <speak2c.emeka@gmail.com>
2020-11-20 14:16:15 +0100

    testing mute

--- src/component/custom/video.js
+++ src/component/custom/video.js
@@ -1,21 +1,33 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Video = ({ peer, name }) => {
-  const refVideo = useRef(null);
-  console.log(peer)
+	const refVideo = useRef(null);
+	const [streamObj, setStreamObj] = useState(null);
 
 	useEffect(() => {
 		if (peer) {
 			peer.on("stream", (stream) => {
+				console.log("stream", stream);
+				setStreamObj(stream);
+				console.log(stream.getVideoTracks());
 				refVideo.current.srcObject = stream;
 			});
 		}
 	}, [peer]);
 
+	const mute = () => {
+		if (streamObj !== null) {
+			streamObj.getVideoTracks()[0].enabled = !streamObj.getVideoTracks()[0]
+				.enabled;
+			streamObj.getAudioTracks()[0].enabled = !streamObj.getAudioTracks()[0]
+				.enabled;
+		}
+	};
+
 	return (
 		<div style={{ display: "flex", flexDirection: "column" }}>
 			<video autoPlay ref={refVideo} playsInline />
-			<span>{name}</span>
+			<span onClick={() => mute()}>mute</span>
 		</div>
 	);
 };
